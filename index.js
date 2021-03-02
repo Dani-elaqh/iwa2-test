2e90795a3e53ba678e50a8dc8d7b81f9b908ee56
Dani-elaqh <ale.quintanillah@gmail.com>
2021-03-02 10:59:18 +0000

    change apis

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const http = require('http');
+const axios = require('axios');
 
 http.createServer((req, res)=>{
-  res.write(users.join("\n")); // write a response
+  res.write(people.join(" \ n ")); //display the list of users on the page
+  //res.write("\n\n"+emails.join(", ")); //display the list of users on the page
   res.end(); //end the response
 }).listen(8000); // listen for requests on port 8000
 
-let users = []; // names of users will be stored here
+let people = []; // names of users will be stored here
+
 (async function getNames(){
   try{
-    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
-    users = data.map(user=>user.name);
-    console.log(users)
+    const {data} = await axios.get("https://swapi.dev/api/people/");
+    //console.log(data);
+    people = data.results.map(user=>user.name);
+    console.log(users);
+    //console.log(homeworld);
   } catch(error){
     console.log(error)
   }
