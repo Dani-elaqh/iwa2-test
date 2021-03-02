504d8551f85f6e9452cdc6c12e00efecc00b1acf
Dani-elaqh <ale.quintanillah@gmail.com>
2021-03-02 09:38:32 +0000

    dennis

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ http.createServer((req, res)=>{
   res.write("Hello BScBest \n"); // write a response
   res.end(); //end the response
 }).listen(8000); // listen for requests on port 8000
+
+let users = []; // names of users will be stored here
+axios.get("https://jsonplaceholder.typicode.com/users")
+.then(({ data }) => {
+    users = data.map(user => user.name); // get only the names of the users and store in an array
+    console.log(users)
+})
+.catch(error=>{
+    console.log(error);
+});
