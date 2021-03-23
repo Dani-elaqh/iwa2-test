2f91807a4b8b82a40522c54fcca93ce32ba0dc5d
Dani-elaqh <ale.quintanillah@gmail.com>
2021-03-23 11:47:21 +0000

    create user

--- user-controller.js
+++ user-controller.js
@@ -1,4 +1,15 @@
-var User = require('./models/user');
+var User = require('./models/user')
+
+exports.createUser = function(req, res) { 
+    var newuser = new User(req.body);
+    newuser.save(function (err, user) { 
+        if (err) { 
+            res.status (400).json(err);
+        }
+
+        res.json(user); 
+});
+};
 
 exports.getUsers = function(req, res) {
   User.find({}, function (err, users) {
