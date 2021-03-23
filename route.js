ab23845f3c9784af285177d528cba8f2efe66857
Dani-elaqh <ale.quintanillah@gmail.com>
2021-03-23 09:28:07 +0000

    iten controller

--- route.js
+++ route.js
@@ -1,18 +1,26 @@
 const express =  require('express'),
 router = express.Router();
 
+var itemCtrl = require('./item-controller');
+
+//refenrencing to where the logic is trough the variable
+router.get('/hello', itemCtrl.getWorld);
+
+router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+
+router.post('/hello', itemCtrl.postWorld);
 
 
 
 //create the routing 
-router.get('/hello/:foo/:bar', (req, res)=>{
-    res.json({message: 'Hello BScBest!', data: [
-        req.params.foo,
-        req.params.bar
-    ]});
-});//end point is hello 
-router.post('/hello', (req,res) => {
-    res.json({result: 'Post was sent', data: req.body});
-});
+// router.get('/hello/:foo/:bar', (req, res)=>{
+//     res.json({message: 'Hello BScBest!', data: [
+//         req.params.foo,
+//         req.params.bar
+//     ]});
+// });//end point is hello 
+// router.post('/hello', (req,res) => {
+//     res.json({result: 'Post was sent', data: req.body});
+// });
 
 module.exports = router;
