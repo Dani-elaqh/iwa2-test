47dca3c454b58d1e813627dcf59a2e47adc968dc
Dani-elaqh <ale.quintanillah@gmail.com>
2021-03-23 11:26:39 +0000

    mongoose

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const axios = require('axios'),
 logger = require('morgan'),
 cors = require ('cors');
 express = require('express'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
 
 var app = express();
 var port = 8000;
@@ -52,10 +53,16 @@ app.use(require('./routes'));
 //     console.log('Listening on port: ' + port)
 // });
 
-const dbURI = "";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then((result) => console.log('connected to db'))
-        .catch((err) => console.log(err));
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
+});
 
+app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
 
