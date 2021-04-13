6e7cd46335f94da97559c8bce09b6f9d7f882bae
Dani-elaqh <ale.quintanillah@gmail.com>
2021-04-13 10:38:09 +0000

    connect db

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ mongoose = require('mongoose');
 var app = express();
 var port = 8000;
 
+
 app.use(bodyParser.json());
 app.use(logger('tiny'));
 app.use(require('./routes'));
@@ -48,7 +49,7 @@ app.listen(port, function(err){
     console.log('Listening on port: ' + port);
 });
 
-const dbURI = "mongodb://localhost/test";
+const dbURI = "mongodb+serv://localhost/test";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((result) => console.log('connected to db'))
